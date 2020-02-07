5c8ddb840830365ca02ac84a80f7f04e95d3b2d3
Lőrincz Péter <peter.pal.lorincz@gmail.com>
2020-02-07 19:04:47 +0100

    Second tab adjusted & disabled resolution setting

---
+++ RuneterraCompanion/BrowserTab.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RuneterraCompanion
+{
+    public partial class BrowserTab : UserControl
+    {
+        private MainWindow mainWindow = null; // Reference to the MainWindow
+
+        public BrowserTab()
+        {
+            InitializeComponent();
+        }
+
+        // get a reference to main windows when it is available.
+        // The Loaded Event is set in the XAML code above.
+        private void OnControlLoaded(object sender, RoutedEventArgs e)
+        {
+            mainWindow = Window.GetWindow(this) as MainWindow;
+        }
+
+        private void AccessMainWindowsWidget()
+        {
+
+        }
+    }
+}
